8585d92ff58a58c792fe7482631cfa6b5e510104
Anurag Dutta <imanurag18@gmail.com>
2022-11-11 09:29:10 +0000

    Create airdrop.js

---
+++ airdrop.js
@@ -0,0 +1,36 @@
+// npm init
+// npm install --save @solana/web3.js
+
+const { 
+  Connection, 
+  LAMPORTS_PER_SOL, 
+  clusterApiUrl, 
+  Keypair
+} = require("@solana/web3.js");
+
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+(async ()=> {
+
+  const keypair = Keypair.generate();
+
+  const airdropSignature = await connection.requestAirdrop(
+    keypair.publicKey,
+    LAMPORTS_PER_SOL
+  );
+  
+  const latestBlockHash = await connection.getLatestBlockhash();
+  
+  const txn = await connection.confirmTransaction({
+    blockhash: latestBlockHash.blockhash,
+    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+    signature: airdropSignature,
+  });
+
+  console.log({
+    publicKey: keypair.publicKey,
+    privateKey: keypair.secretKey,
+    signature: airdropSignature,
+    txn
+  })
+})()
